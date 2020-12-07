1f6931efbb9eefbeb77f66045fc8df44ebd72da0
Abdelwahab0 <abdelwahabahmed0@outlook.com>
2020-12-07 20:52:20 +0200

    added Likes and Comments and delete comments, also implemented subscribtion and (likesCount, commentCount)

--- src/graphql/typeDefs.js
+++ src/graphql/typeDefs.js
@@ -1,11 +1,26 @@
 const gql = require('graphql-tag');
 
 module.exports = gql`
-  type Post{
-  id: ID!
-  body: String!
-  createdAt: String!
-  username: String!
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+    comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
+  }
+  type Comment{
+    id: ID!
+    username: String!
+    body: String!
+    createdAt: String!
+  }
+  type Like{
+    id: ID!
+    createdAt: String!
+    username: String!
   }
   input RegisterInput{
     username: String!
@@ -29,5 +44,11 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
+  }
+  type Subscription{
+    newPost: Post!
   }
 `;
