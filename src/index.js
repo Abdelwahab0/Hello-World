ca5bb8b6e3473fef5ac6f332127f6cd30ee7d5ea
Abdelwahab0 <abdelwahabahmed0@outlook.com>
2020-12-07 04:19:33 +0200

    setting up ApolloServer and connecting to database

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require('apollo-server');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers/index');
+const { MONGODB } = require('./config');
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req }),
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('MongoDB Connected');
+    return server.listen({ port: 5000 });
+  }).then((res) => {
+  // eslint-disable-next-line no-console
+    console.log(`Server running at ${res.url}`);
+  });
